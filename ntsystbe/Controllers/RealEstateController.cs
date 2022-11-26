77df06df21f89536b8f3de23118797154c8b81a3
JuZeko <zekonis.julius1@gmail.com>
2022-11-26 22:17:35 +0200

    Add project files.

---
+++ ntsystbe/Controllers/RealEstateController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using ntsystbe.Dto;
+
+namespace ntsystbe.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RealEstateController : ControllerBase
+    {
+
+        [HttpPost]
+        public void Get([FromBody] RealEstateDto realEstateDto)
+        {
+            var smth = realEstateDto;
+        }
+
+    }
+}
---
+++ ntsystbe/Dto/RealEstate.cs
@@ -0,0 +1,12 @@
+namespace ntsystbe.Dto
+{
+    public class RealEstateDto
+    {
+        public string? kaina { get; set; }
+        public string? statybosMetai { get; set; }
+        public string? pastatoTypas { get; set; }
+
+        public string? plotas { get; set; }
+        public string? sildimoTypas { get; set; }
+    }
+}
